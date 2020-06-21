1923d90d255ed7687531b66b357a7a32cbc8a506
Daniel_B <daniel.bogucki2@gmail.com>
2020-06-21 23:40:14 +0200

    add container for chart

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
 import {version} from '../../../../package.json';
 
 @Component({
@@ -6,13 +6,29 @@ import {version} from '../../../../package.json';
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent implements OnInit {
-  public appVersion: string;
+export class DashboardComponent implements OnInit, AfterViewInit {
+  appVersion: string;
 
   constructor() {
   }
 
   ngOnInit(): void {
     this.appVersion = version;
+  }
+
+  ngAfterViewInit(): void {
+    this.calculateChartContainerHeight();
+  }
+
+  onWindowResize() {
+    this.calculateChartContainerHeight();
+  }
+
+  calculateChartContainerHeight() {
+    const headerHeight = parseInt(window.getComputedStyle(document.querySelector('header')).height, 10);
+    const footerHeight = parseInt(window.getComputedStyle(document.querySelector('footer')).height, 10);
+
+    const chartContainerElement: HTMLElement = document.querySelector('#chart-container');
+    chartContainerElement.style.height = `${window.innerHeight - (headerHeight + footerHeight)}px`;
   }
 }
